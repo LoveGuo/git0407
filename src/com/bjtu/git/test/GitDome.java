72af59be0e4cd851f172de511ee699225877aed2
LoveGuo <1175306620@qq.com>
2016-10-13 23:44:00 +0800

    git synchronizing
    git synchronizing

--- src/com/bjtu/git/test/GitDome.java
+++ src/com/bjtu/git/test/GitDome.java
@@ -4,5 +4,6 @@ public class GitDome {
 	public static void main(String[] args) {
 		System.out.println("hello 802");
 		System.out.println("gitHub change");
+		System.out.println("git synchronizing");
 	}
 }
